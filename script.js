718c716320a81e2a0153840a4c94ca344fb8924c
Haley Morris <haleymor@smartt.email>
2020-02-10 16:12:41 -0500

    added new assetimage, added minimal styling, questions now cycle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const logoQuiz = {};
-const logos = [
+const myApp = {};
+myApp.logos = [
     {
         logo: "assets/atari.png",
         choices: {
@@ -52,67 +52,72 @@ const logos = [
     },
 ];
 
-const quizQuestions = logos.map(function(currentQuestion) {
+// creates a new array populated with the results of calling currentQuestion on every element in the calling array.
+const quizQuestions = myApp.logos.map(function(currentQuestion, index) {
     const question = `
         <div class="imgLogo">
             <img src="${currentQuestion.logo}" alt="">
         </div>
         <div class="choices">
             <div class="choice1">
-                <input type="radio" name="aChoice" class="selectedAnswer" value="a">
+                <input type="radio" data-index="${index}" name="achoice" class="selectedAnswer" value="a">
                 <label for="a">${currentQuestion.choices.a}</label>
             </div>
             <div class="choice2">
-                <input type="radio" name="bChoice" class="selectedAnswer" value="b">
+                <input type="radio" data-index="${index}" name="bchoice" class="selectedAnswer" value="b">
                 <label for="b">${currentQuestion.choices.b}</label>
             </div>
             <div class="choice3">
-                <input type="radio" name="cChoice" class="selectedAnswer" value="c">
+                <input type="radio" data-index="${index}" name="cChoice" class="selectedAnswer" value="c">
                 <label for="$c">${currentQuestion.choices.c}</label>
             </div>
             <div class="choice4">
-                <input type="radio" name="dChoice" class="selectedAnswer" value="d">
+                <input type="radio" data-index="${index}" name="dChoice" class="selectedAnswer" value="d">
                 <label for="d">${currentQuestion.choices.d}</label>
             </div>
         </div>`;
     return question;
 });
 
+let score = 0;
 
-logoQuiz.init = function () {
+myApp.init = function () {
     // on page load have event listener that listens for click on button
-    $('a').on('click', function(e) {
+    $('.button').on('click', function(e) {
         e.preventDefault();
         // when click event is fired have html for logo 1 be added to the page
         $('.card').html(quizQuestions[0]);
     });
-
     // then listen for change event on radio button
     $('.card').on('change', 'input', function() {
         // only one radio button can be selected at one time
         $('.selectedAnswer').not(this).prop('checked', false);
         // store users choice as letter 
         const userChoice = $(this).val();
+        // store current question index in order to update current question variable
+        const currentQuestionIndex = $(this).data('index');
         // starting score should be zero
-        let score = 0;
-        for (let i = 0; i < logos.length; i++)
-            //compare if userChoice is equal to correctAnswer
-            if (userChoice === logos[i].correctAnswer) {
-                //if correct add 1 to score and have question 2 come up
-                score++;
-                $('.card').html(logos[i++]);
-            } else {
-                // if inccorrect just have question 2 come up
-                $('.card').html(logos[i++]);
-            }
-        console.log(score);
+        // let score = 0;
+        //compare if userChoice is equal to correctAnswer
+        if (userChoice === myApp.logos[currentQuestionIndex].correctAnswer && currentQuestionIndex <= 3) {
+            //if correct add 1 to score and have next come up
+            score++;
+            $('.card').html(quizQuestions[currentQuestionIndex + 1]);
+            console.log(score);
+        } else if (userChoice !== myApp.logos[currentQuestionIndex].correctAnswer && currentQuestionIndex <= 3) {
+            // if inccorrect just have next question come up
+            $('.card').html(quizQuestions[currentQuestionIndex + 1]);
+        } else
+            $('.card').html(`<h2>Your score is ${score} / 5 </h2>`);
     });
 };
 
-
 $(function () {
-    logoQuiz.init();
+    myApp.init();
 });
 //     update current question variable
+        // implement an attribute on each radio button called dataQuestion, for first question this would be zero
+        //then have correct answer be compared against userChoice directly 
+
 // repeat above for next change event until go through all logos, if current question variable = 5 then display results by appending html
 // listen for click on try again button to restart entire game(maybe stretch)
